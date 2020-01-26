3dbe86e4a08b3df429b00d7d2217aaa08f42303e
andersbuck <buckles.anderson@gmail.com>
2020-01-26 17:52:26 -0500

    Configure to scrape fb mkt

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from selenium import webdriver
 import os
+import sys
+
+# Configure Selenium 
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-# Now you can start using Selenium
+chrome_driver = None
 
-driver.get("https://mobile.facebook.com/marketplace/charlotte/?radius_in_km=161&query=jeep+cherokee")
-#driver.get("https://www.facebook.com/marketplace/charlotte/vehicles/?vehicleMake=Jeep&vehicleModel=Jeep%20Cherokee&minVehicleYear=1997&maxVehicleYear=2001&sort=CREATION_TIME_DESCEND")
-#print(driver.page_source)
-content = driver.find_element_by_css_selector('#u_0_24')
-print(content.text)
+isHerokuEnv = sys.argv[1]
+
+if isHerokuEnv=='true':
+    chrome_driver = os.environ.get("CHROMEDRIVER_PATH")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+else:
+    chrome_driver = os.getcwd() + "/chromedriver_linux64/chromedriver"
+
+driver = webdriver.Chrome(executable_path=chrome_driver, chrome_options=chrome_options)
+
+# Start Selenium script
+
+driver.get("https://www.facebook.com/marketplace/charlotte/vehicles/?vehicleMake=Jeep&vehicleModel=Jeep%20Cherokee&minVehicleYear=1997&maxVehicleYear=2001&sort=CREATION_TIME_DESCEND")
+driver.implicitly_wait(10)
+content = driver.find_element_by_css_selector('img._7ye')
+print(content.get_attribute('alt'))
